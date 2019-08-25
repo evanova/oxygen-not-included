084c30c57b495607a4ee37e4965278ecaeaa1d27
Evanova <evanova.mobile@gmail.com>
2019-08-25 13:30:49 -0400

    Initial Commit

---
+++ src/BuildingAutoSwitch/BuildingAutoSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Harmony;
+
+namespace BuildingAutoSwitch {
+
+    public class BuildingAutoSwitch 
+    {
+        
+        [HarmonyPatch(typeof(Toggleable))]
+        [HarmonyPatch("QueueToggle")]
+        [HarmonyPatch(new Type[] { typeof(int) })]
+        public static class ToggleablePatch 
+        {
+            [HarmonyPrefix]
+            public static bool TogglePatch(Toggleable __instance, ref int targetIdx) 
+            {
+                if (__instance.IsToggleQueued(targetIdx)) 
+                {
+                    return false;
+                }
+
+                FieldInfo info = __instance.GetType().GetField(
+                    "targets",
+                    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                
+                List<KeyValuePair<IToggleHandler, Chore>> targets = info.GetValue(__instance) as List<KeyValuePair<IToggleHandler, Chore>>;
+                targets[targetIdx].Key.HandleToggle();
+                return false;
+            }
+        }
+    }
+}
---
+++ src/BuildingAutoSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildingAutoSwitch")]
+[assembly: AssemblyDescription("Enable and disable buildings without waiting for a Dup to do it.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuildingAutoSwitch")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("284386B8-AE97-4588-931F-669581F9C708")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
